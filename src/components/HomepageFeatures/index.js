e7fe9d0626c749e6f365bfc280a846d31330c5f0
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2022-12-20 15:08:07 -0600

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -8,28 +8,25 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+       Laravel Workflow uses simple and intuitive syntax, with clear conventions and a clean API.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Monitoring and Control',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Complex and dynamic control over the execution of jobs. Monitor the progress and status of the workflow as a whole.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Powered by Laravel',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Uses its queue system and database layer to store and manage workflow data and state.
       </>
     ),
   },
