df346f5139447951578127109ebe6df396be556e
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2022-12-20 18:49:20 -0600

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,7 +94,7 @@ const config = {
               },
               {
                 label: 'Installation',
-                to: '/docs/install',
+                to: '/docs/intro',
               },
             ],
           },
