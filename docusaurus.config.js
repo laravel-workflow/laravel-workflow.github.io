007a4c22aab880f139423df2e523b79ce0aeb36f
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2022-12-20 20:39:07 -0600

    Add docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/laravel-workflow/laravel-workflow.github.io/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/laravel-workflow/laravel-workflow.github.io/blob/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/laravel-workflow/laravel-workflow.github.io/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/laravel-workflow/laravel-workflow.github.io/blob/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
