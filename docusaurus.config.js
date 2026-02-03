803a24510e4e614561215d33449010a6f6c5001e
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2026-02-03 11:32:53 -0600

    Add Google Analytics tracking configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-HD1YHT442Y',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
